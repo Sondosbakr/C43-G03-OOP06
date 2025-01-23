323269a225dd156ba05fd42e0900f3ac50597692
sondos bakr <sondosbak81r@gmail.com>
2025-01-23 13:52:40 +0200

    Add project files.

---
+++ Assignment06OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace Assignment06OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            
+        }
+    }
+
+    
+}
